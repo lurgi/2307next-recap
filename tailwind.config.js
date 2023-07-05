82da311b213907c07e6922748b15e5a6bbfdc239
lurgi <wahoo9040@naver.com>
2023-07-05 12:18:28 +0900

    next recap start! (link fix)

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{ts,js,jsx,tsx}",
+    "./components/**/*.{ts,js,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
