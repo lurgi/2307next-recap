f3d2b5db2951090fcc6519df083db7dfa983d09c
lurgi <wahoo9040@naver.com>
2023-07-06 09:19:47 +0900

    connect prisma

---
+++ pages/api/client-text.tsx
@@ -0,0 +1,16 @@
+import client from "@/libs/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  await client.user.create({
+    data: {
+      name: "hi",
+    },
+  });
+  res.json({
+    ok: true,
+  });
+}
