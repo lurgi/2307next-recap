f04fb8da8349f6b4d69e34f84e9bbacb5810ef7d
lurgi <wahoo9040@naver.com>
2023-07-07 14:43:20 +0900

    confirm token

--- pages/api/users/confirm.tsx
+++ pages/api/users/confirm.tsx
@@ -1,9 +1,24 @@
+import { withIronSessionApiRoute } from "iron-session/next";
 import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
+import client from "@/libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { token } = req.body;
+  const exists = await client.token.findUnique({
+    where: {
+      payload: token,
+    },
+  });
+  if (!exists) res.status(400).end();
+  req.session.user = {
+    id: exists?.userId,
+  };
+  await req.session.save();
   return res.status(200).end();
 }
 
-export default withHandler("POST", handler);
+export default withIronSessionApiRoute(withHandler("POST", handler), {
+  cookieName: "carrotsession",
+  password: process.env.SESSION_PASS!,
+});
